4427c2395a309fe2160588658732e28d22a60e72
genericptr <genericptr@gmail.com>
2015-03-10 09:14:29 +0700

    Initial commit (still being corrected)

---
+++ DefinedClassesDiscRecording.pas
@@ -0,0 +1,24 @@
+{$mode delphi}
+{$modeswitch objectivec1}
+{$modeswitch cvar}
+
+unit DefinedClassesDiscRecording;
+interface
+
+type
+  DRBurn = objcclass external;
+  DRCDTextBlock = objcclass external;
+  DRDevice = objcclass external;
+  DRErase = objcclass external;
+  DRFSObject = objcclass external;
+  DRFile = objcclass external;
+  DRFolder = objcclass external;
+  DRMSF = objcclass external;
+  DRMSFFormatter = objcclass external;
+  DRNotificationCenter = objcclass external;
+  DRTrack = objcclass external;
+  DRFileDataProductionProtocol = objcprotocol external name 'DRFileDataProduction';
+  DRTrackDataProductionProtocol = objcprotocol external name 'DRTrackDataProduction';
+
+implementation
+end.
---
+++ DefinedClassesDiscRecordingUI.pas
@@ -0,0 +1,16 @@
+{$mode delphi}
+{$modeswitch objectivec1}
+{$modeswitch cvar}
+
+unit DefinedClassesDiscRecordingUI;
+interface
+
+type
+  DRBurnProgressPanel = objcclass external;
+  DRBurnSetupPanel = objcclass external;
+  DREraseProgressPanel = objcclass external;
+  DREraseSetupPanel = objcclass external;
+  DRSetupPanel = objcclass external;
+
+implementation
+end.
